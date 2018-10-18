7885916f49a1dd256e8530555ba4a9f6a1043f67
Abigail Lu <abigailitliong.lu@calbaptist.edu>
2018-10-18 01:06:17 -0700

    started set

--- src/test/java/TestArrayList.java
+++ src/test/java/TestArrayList.java
@@ -3,17 +3,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TestArrayList {
 
     private ArrayList<Integer> aList = new ArrayList<Integer>();
+    private Set<Integer> aSet = new HashSet<Integer>();
     private int STARTING_SIZE = 0;
 
+    /**
+     * Start each test with 3 values each collection.
+     */
     @BeforeEach
     public void init() {
         aList.add(1);
         aList.add(2);
         aList.add(3);
+
+        aSet.add(1);
+        aSet.add(2);
+        aSet.add(3);
 
         STARTING_SIZE = aList.size();
     }
@@ -23,32 +33,58 @@ public class TestArrayList {
      */
     @Test
     public void size() {
+        /*** ARRAY LIST ***/
         Assertions.assertEquals(STARTING_SIZE, aList.size());
+
+        /*** SET ***/
+        Assertions.assertEquals(3, aSet.size());
+
     }
 
     /**
-     * Test if add method works in Array List.
-     * <p>
+     * Test if add method works
      * Make sure that an integer is added to the list.
      * Make sure that the size of the list increases.
      * Make sure you can only add Integers.
      */
     @Test
     public void add() {
+
+        ArrayList<Integer> tmpArrayList = new ArrayList<Integer>();
+        Set<Integer> tmpSet = new HashSet<Integer>();
+
+        /*** ARRAY LIST ***/
         Assertions.assertTrue(STARTING_SIZE == 3);
         Assertions.assertTrue(aList.add(4));
         Assertions.assertSame(STARTING_SIZE + 1, aList.size());
         // Assertions.assertFalse(aList.add("one")); <-- intellij already catches this as an error
 
-        ArrayList<Integer> tmp = new ArrayList<Integer>();
-        tmp.add(1);
-        tmp.add(2);
-        Assertions.assertTrue(aList.addAll(tmp));
+        tmpArrayList.add(1);
+        tmpArrayList.add(2);
+        Assertions.assertTrue(aList.addAll(tmpArrayList)); // can add same int in array list
+        Assertions.assertFalse(aList.addAll(tmpSet)); // can't add different collection into array list
         Assertions.assertSame(STARTING_SIZE + 3, aList.size());
+
+        /*** SET ***/
+        Assertions.assertTrue(aSet.add(4));
+        Assertions.assertSame(STARTING_SIZE + 1, aSet.size());
+
+        tmpSet.add(1);
+        tmpSet.add(2);
+        Assertions.assertFalse(aSet.addAll(tmpSet)); // can't add array list into set
+        Assertions.assertFalse(aSet.addAll(tmpArrayList)); // can't add same int in set
+
+        tmpSet.clear();
+        tmpSet.add(5);
+        tmpSet.add(6);
+        Assertions.assertTrue(aSet.addAll(tmpSet));
+        Assertions.assertSame(STARTING_SIZE + 3, aSet.size());
+
+
     }
 
     /**
-     * Test if remove method works in Array List.
+     * Test if remove method works
      * Make sure that an integer is removed from the list.
      * Make sure that the you are able to remove an integer from an index.
      * make sure that you are able to remove all objects in list.
@@ -56,6 +92,7 @@ public class TestArrayList {
      */
     @Test
     public void remove() {
+        /*** ARRAY LIST ***/
         Assertions.assertTrue(aList.remove((Integer) 1));
         Assertions.assertEquals(STARTING_SIZE - 1, aList.size());
         Assertions.assertEquals(2, (int)aList.remove(0)); // have to cast to differentiate between removing index or object
@@ -67,10 +104,11 @@ public class TestArrayList {
     }
 
     /**
-     * Test if clear method works in Array List.
+     * Test if clear method works
      */
     @Test
     public void clear() {
+        /*** ARRAY LIST ***/
         Assertions.assertTrue(aList.size() > 0);
         Assertions.assertArrayEquals(new Integer[]{1,2,3}, aList.toArray()); // can't do int
     }
